396632275ccffeeec5925a29f2cbf40cae462879
Marija Kalebota <marija.kalebota@gmail.com>
2018-02-16 12:42:43 +0100

    Adjust HookeJeeves so that it works for 2D points. Adjust drawAnimation so that it takes the width and heigth of the desired graph, as well as the accuracy of the graph (as number of samples of points from the domain range), as arguments. Add method drawAnimationContour.

--- InteractiveOptimisationAlgorithms/Test0.py
+++ InteractiveOptimisationAlgorithms/Test0.py
@@ -15,6 +15,8 @@ from IPython.utils.py3compat import annotate
 from IPython.display import display
 #from ipywidgets import FloatSlider
 from ipywidgets import *
+import numbers
+import decimal
 #class IFunction:
 
 '''
@@ -27,7 +29,15 @@ class Iteration():
     def __init__(self, iterationNumber, yValue, xValue, additionalInfo):
         self.iterationNumber = iterationNumber
         self.yValue = yValue
-        self.xValue = xValue
+        #self.xValue = xValue
+        #if(isinstance(xValue, float)):
+        if (isinstance(xValue, numbers.Number)):
+            self.xValue = xValue
+            self.x1Value = xValue
+            self.x2Value = xValue
+        else:
+            self.x1Value = xValue.getElements()[0,0]
+            self.x2Value = xValue.getElements()[0,1]
         self.additionalInfo = additionalInfo
 
 class Logger(object):
@@ -76,15 +86,45 @@ class Drawer:
     def __init__(self, logger):
         self.logger = logger
 
-    def drawAnimation(self):
+    #def drawAnimation(self):
+    def drawAnimation(self, minX, maxX, minY, maxY, numberOfSamplesOfX):
         playMaxOfInterval = 0
         poljeX = []
         poljeY = []
+        '''
+        minX = self.logger.getIterations()[0].xValue
+        maxX = self.logger.getIterations()[0].xValue
+        minY = self.logger.getIterations()[0].yValue
+        maxY = self.logger.getIterations()[0].yValue
+        '''
+
         for iteration in self.logger.getIterations():
             playMaxOfInterval = playMaxOfInterval + 1
             # stvori polje ovih brojeva
             poljeX.append(iteration.xValue)
+            '''
+            if(iteration.xValue < minX):
+                minX = iteration.xValue
+            if (iteration.xValue > maxX):
+                maxX = iteration.xValue
+            if (iteration.yValue < minY):
+                minY = iteration.yValue
+            if (iteration.yValue > maxY):
+                maxY = iteration.yValue
+            '''
+
             poljeY.append(iteration.yValue)
+        '''
+        print "PoljeX: "
+        for x in poljeX:
+            print str(x)
+        print "PoljeY: "
+        for x in poljeY:
+            print str(x)
+        '''
+
+        relativniOdmakX = (maxX - minX) / 10
+        relativniOdmaky = (maxY - minY) / 10
 
         w = widgets.IntSlider(min=0, max=playMaxOfInterval - 1, step=1, value=0)
         play = widgets.Play(
@@ -110,18 +150,25 @@ class Drawer:
         previousButton.on_click(on_previousButton_clicked)
 
         widgets.jslink((play, 'value'), (w, 'value'))
-        def f(poljeX, poljeY, iteration):
+        #def f(poljeX, poljeY, iteration):
+        #def f(poljeX, poljeY, iteration, minX, maxX, minY, maxY, relOdmakX, relOdmakY):
+        def f(poljeX, poljeY, iteration, minX, maxX, minY, maxY):
             funkcija = self.logger.getFunction()
             plt.clf()
             plt.close('all')
             plt.figure(iteration)
-            plt.axis([0.0, 15.0, -5.0, 30.0])
+            #plt.axis([0.0, 15.0, -5.0, 30.0])
             #plt.axis([0.0, 6.0, -0.5, 4.0])
             #TODO maknuti hardkodirani axis - postaviti ga na (min(x) - relativni odmak),(max(x) + relativni odmak), isto za y
+            #plt.axis([minX - relOdmakX, maxX + relOdmakX, minY - relOdmakY, maxY + relOdmakY])
+            plt.axis([minX, maxX, minY, maxY])
             ax = plt.gca()
             ax.set_autoscale_on(False)
 
-            X = np.linspace(0.0, 13.0, num=10)
+            #X = np.linspace(0.0, 13.0, num=10)
+
+            X = np.linspace(minX, maxX, num=numberOfSamplesOfX)
+            #X = np.linspace(minX - relOdmakX, maxX + relOdmakX, num=10)
             #X = np.linspace(0.0, 6.0, num=10)
             #TODO ovaj linspace staviti na iste vrijednosti kao i axis
             Y = [funkcija.valueAt(x) for x in X]
@@ -130,7 +177,130 @@ class Drawer:
             plt.plot(poljeX[iteration], poljeY[iteration], 'ro')
             plt.show()
 
-        interact(f, poljeX=fixed(poljeX), poljeY = fixed(poljeY), iteration=w)
+        #interact(f, poljeX=fixed(poljeX), poljeY = fixed(poljeY), iteration=w)
+        #interact(f, poljeX=fixed(poljeX), poljeY=fixed(poljeY), iteration=w, minX=fixed(minX), maxX=fixed(maxX), minY=fixed(minY), maxY=fixed(maxY), relOdmakX = fixed(relativniOdmakX), relOdmakY = fixed(relativniOdmaky))
+        interact(f, poljeX=fixed(poljeX), poljeY=fixed(poljeY), iteration=w, minX=minX, maxX=maxX, minY=minY, maxY=maxY)
+        display(play)
+
+        display(previousButton)
+        display(nextButton)
+
+    #def drawAnimationContour(self):
+    def drawAnimationContour(self, minX, maxX, minY, maxY, numberOfSamplesOfX):
+        playMaxOfInterval = 0
+        poljeX = []
+        poljeY = []
+
+        for iteration in self.logger.getIterations():
+            playMaxOfInterval = playMaxOfInterval + 1
+            # stvori polje ovih brojeva
+            #poljeX.append(iteration.xValue)
+            #poljeX.append(iteration.xValue)
+            poljeX.append(iteration.x1Value)
+            #poljeX.append(iteration.xValue.getElements()[0,0])
+            #poljeY.append(iteration.yValue)
+            poljeY.append(iteration.x2Value)
+            #poljeY.append(iteration.yValue)
+
+        '''
+        print "PoljeX: "
+        for x in poljeX:
+            print str(x)
+        print "PoljeY: "
+        for x in poljeY:
+            print str(x)
+        '''
+
+        w = widgets.IntSlider(min=0, max=playMaxOfInterval - 1, step=1, value=0)
+        play = widgets.Play(
+            value=0,
+            min=0,
+            max=playMaxOfInterval,
+            step=1,
+            description="Press play",
+            disabled=False
+        )
+        nextButton = widgets.Button(description="Next")
+        previousButton = widgets.Button(description="Previous")
+
+        def on_nextButton_clicked(x):
+            if (play.value < play.max):
+                play.value += 1
+
+        def on_previousButton_clicked(x):
+            if (play.value > 0):
+                play.value -= 1
+
+        nextButton.on_click(on_nextButton_clicked)
+        previousButton.on_click(on_previousButton_clicked)
+
+        widgets.jslink((play, 'value'), (w, 'value'))
+
+        def f(poljeX, poljeY, poljeZ, xRjesenja, yRjesenja, iteration):
+        #def f(poljeX, poljeY, poljeZ, xRjesenja, yRjesenja, iteration, minX, maxX, minY, maxY):
+            # def f(poljeX, poljeY, iteration, minX, maxX, minY, maxY, relOdmakX, relOdmakY):
+            funkcija = self.logger.getFunction()
+            plt.clf()
+            plt.close('all')
+            plt.figure(iteration)
+            #plt.axis([-6.0, 6.0, -6.0, 6.0])
+            # plt.axis([0.0, 6.0, -0.5, 4.0])
+            # TODO maknuti hardkodirani axis - postaviti ga na (min(x) - relativni odmak),(max(x) + relativni odmak), isto za y
+            # plt.axis([minX - relOdmakX, maxX + relOdmakX, minY - relOdmakY, maxY + relOdmakY])
+            plt.axis([minX, maxX, minY, maxY])
+            ax = plt.gca()
+            ax.set_autoscale_on(False)
+
+            #X = np.linspace(-6.0, 6.0, num=10)
+            X = np.linspace(minX, maxX, num=numberOfSamplesOfX)
+            # X = np.linspace(minX - relOdmakX, maxX + relOdmakX, num=10)
+            # X = np.linspace(0.0, 6.0, num=10)
+            # TODO ovaj linspace staviti na iste vrijednosti kao i axis
+            Y = [funkcija.valueAt(x) for x in X]
+
+            plt.contourf(poljeX, poljeY, poljeZ, 20, cmap='RdGy')
+            plt.colorbar();
+
+            #plt.plot(X, Y, 'b')
+            #plt.plot(poljeX[iteration], poljeY[iteration], 'bo')
+            plt.plot(xRjesenja[iteration], yRjesenja[iteration], 'go')
+            plt.show()
+
+
+        def rosenbrock(x, y):
+            # return np.sin(np.sqrt(x ** 2 + y ** 2))
+            return 100 * (y - x ** 2) ** 2 + (1 - x) ** 2
+
+        #TODO get file name as argument, or from logger!
+        thisFile = open('HookeJeevesOutput.txt', 'r')
+        lines = thisFile.readlines()
+        # print lines
+        for i in range(len(lines)):
+            lines[i] = lines[i].split()
+            # lines[i] = lines[i][0].split(" ")
+        # print lines
+        thisFile.close()
+        rjesenja = lines
+
+        xRjesenja = []
+        yRjesenja = []
+
+        for rjesenje in rjesenja:
+            xRjesenja.append(float(rjesenje[0]))
+            yRjesenja.append(float(rjesenje[1]))
+
+        #x = np.linspace(-6, 6, 30)
+        x = np.linspace(minX, maxX, 30)
+        #y = np.linspace(-6, 6, 30)
+        y = np.linspace(minY, maxY, 30)
+
+        X, Y = np.meshgrid(x, y)
+        Z = rosenbrock(X, Y)
+
+        #interact(f, poljeX=fixed(X), poljeY=fixed(Y), poljeZ = fixed(Z), iteration=w)
+        # interact(f, poljeX=fixed(poljeX), poljeY=fixed(poljeY), iteration=w, minX=fixed(minX), maxX=fixed(maxX), minY=fixed(minY), maxY=fixed(maxY), relOdmakX = fixed(relativniOdmakX), relOdmakY = fixed(relativniOdmaky))
+        interact(f, poljeX=fixed(X), poljeY=fixed(Y), poljeZ=fixed(Z), xRjesenja = fixed(xRjesenja), yRjesenja = fixed(yRjesenja), iteration=w)
+        #interact(f, poljeX=fixed(X), poljeY=fixed(Y), poljeZ=fixed(Z), xRjesenja=fixed(xRjesenja), yRjesenja=fixed(yRjesenja), iteration=w, minX = minX, maxX = maxX, minY = minY, maxY = maxY)
         display(play)
 
         display(previousButton)
@@ -343,7 +513,11 @@ class HookeJeeves:
             additionalInfo["xn"] = xnTuple
 
             #currentIteration = Iteration(iteracija, f.valueAt(xn), xn, additionalInfo)
-            currentIteration = Iteration(iteracija, f.valueAt(xn), xn.getElement(0, 0), additionalInfo)
+            #currentIteration = Iteration(iteracija, f.valueAt(xn), xn.getElement(0, 0), additionalInfo)
+            if(xn.getColsCount() == 1):
+                currentIteration = Iteration(iteracija, f.valueAt(xn), xn.getElement(0, 0), additionalInfo)
+            elif(xn.getColsCount() == 2):
+                currentIteration = Iteration(iteracija, f.valueAt(xn), xn, additionalInfo)
             logger.addIteration(currentIteration)
 
             if (f.valueAt(xn) < f.valueAt(xb)):
