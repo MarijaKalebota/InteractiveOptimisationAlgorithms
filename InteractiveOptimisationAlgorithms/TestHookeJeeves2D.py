018ec24daf468149c81b91eaea47e868591e6d21
Marija Kalebota <marija.kalebota@gmail.com>
2018-03-11 18:13:27 +0100

    Update imports and method calls for compatibility with changes to class HookeJeeves. Fix circular imports.

--- InteractiveOptimisationAlgorithms/TestHookeJeeves2D.py
+++ InteractiveOptimisationAlgorithms/TestHookeJeeves2D.py
@@ -3,11 +3,13 @@ import numpy as np
 from Matrix import *
 #import Functions
 from Functions import *
-from Test0 import *
 from Algorithms import *
+from Drawing import *
 
 
 f1 = F1RosenbrockBananaFunction()
 elements1 = np.array([[-1.9, 2]])
 point1 =  Matrix(1, 2, elements1)
-solutionHookeJeeves1, logger = HookeJeeves.run(f1, point1, 1, 0.5, 1E-6, True)
+h_j_algorithm = HookeJeeves(f1, 1, 0.5, 1E-6, True)
+#solutionHookeJeeves1, logger = HookeJeeves.run(f1, point1, 1, 0.5, 1E-6, True)
+solutionHookeJeeves1, logger = h_j_algorithm.run(point1)
