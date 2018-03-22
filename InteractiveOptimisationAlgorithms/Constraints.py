17303e81229127434d3e946a33fa5aed7f0bb511
Marija Kalebota <marija.kalebota@gmail.com>
2018-03-22 10:58:51 +0100

    Create classes IConstraint and ImplicitConstraint1

---
+++ InteractiveOptimisationAlgorithms/Constraints.py
@@ -0,0 +1,26 @@
+from Matrix import *
+
+class IConstraint(object):
+
+    def is_satisfied(self, point):
+        raise NotImplementedError
+
+    def value_at(self, point):
+        raise NotImplementedError
+
+    def get_gradient(self):
+        raise NotImplementedError
+
+
+class ImplicitConstraint1(IConstraint):
+    def is_satisfied(self, point):
+        if(point.getElement(0,1) - point.getElement(0,0) >= 0):
+            return True
+        else:
+            return False
+
+    def value_at(self, point):
+        if(self.is_satisfied(point)):
+            return point.getElement(0,1) - point.getElement(0,0)
+        else:
+            return float('inf')
--- InteractiveOptimisationAlgorithms/__init__.py
+++ InteractiveOptimisationAlgorithms/__init__.py
@@ -2,3 +2,4 @@ import Functions
 import Matrix
 import Drawing
 import Algorithms
+import Constraints
